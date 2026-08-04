bfaa55aabc119e75923d967f063dadcea2e960a9
singhdipanshusingh46-create <singhdipanshusingh46@gmail.com>
2026-08-04 07:10:26 +0530

    Create 01_while.c

---
+++ while_loop.c/01_while.c
@@ -0,0 +1,14 @@
+/*Question:  
+Write a C program to print numbers from 1 to 100 using a while loop.*/
+
+#include<stdio.h>
+int main(){
+    int i=1;//whlie loop mai aap intialization while loop ke bahar bhi likh sakte hai.
+    while(i<101){
+    printf("%d\n",i);
+    i=i+1;
+    
+    }
+    return 0; 
+    
+}
