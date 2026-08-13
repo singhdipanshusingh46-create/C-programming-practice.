7f38515b245dc20916ea9e94e88195dd20c7433c
singhdipanshusingh46-create <singhdipanshusingh46@gmail.com>
2026-08-13 20:19:07 +0530

    Create 10_pattern_printing.c

---
+++ pattern printing/10_pattern_printing.c
@@ -0,0 +1,21 @@
+/*Question: WAP to print a given pattern.
+   *
+  **
+ ***
+****   */
+#include<stdio.h>
+int main(){
+    for(int i=1; i<=4; i++){
+        for(int j=1; j<=4-i; j++){
+            printf(" ");
+        }
+       for(int k=1; k<=i; k++){
+            printf("*");
+
+       }
+        printf("\n");
+    }
+
+
+    return 0;
+}
