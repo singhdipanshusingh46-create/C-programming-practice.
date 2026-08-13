d0847f1230e6ebfc6692089ec9b0203383a87111
singhdipanshusingh46-create <singhdipanshusingh46@gmail.com>
2026-08-13 20:16:10 +0530

    Create 09_pattern_printing.c

---
+++ pattern printing/09_pattern_printing.c
@@ -0,0 +1,26 @@
+/*Question: WAP to print a given pattern.
+1
+0 1 
+1 0 1 
+0 1 0 1 
+1 0 1 0 1 
+ */
+
+#include<stdio.h>
+int main(){
+    int n;
+    int a=0;
+    printf("enter a number:");
+    scanf("%d",&n);
+   for(int i=1; i<=n; i++){
+
+       for(int j=1; j<=i; j++){
+        if((i+j)%2==0)printf("1 ");
+        else printf("0 ");
+
+       }
+       printf("\n");
+   }
+
+    return 0;
+}
